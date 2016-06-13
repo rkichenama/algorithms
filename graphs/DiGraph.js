fecf88aa8047936d6a95adda708fd80949e4f02b
Richard Kichenama <rkichenama@gmail.com>
2016-06-13 12:33:13 -0400

    graph data types

---
+++ graphs/DiGraph.js
@@ -0,0 +1,26 @@
+class DiGraph /*extends Graph*/ {
+  /*
+  create a V-vertex graph with no edges
+  */
+  constructor (V) {}
+  // number of vertices
+  V () {}
+  // number of edges
+  E () {}
+  /*
+  add edge v->w to digraph
+  @param {object} v
+  @param {object} w
+  */
+  addEdge (v, w) {}
+  /*
+  vertices adjacent to v
+  @param {object} v
+  */
+  adj (v) {}
+  // reverse the digraph
+  reverse () {}
+  toString () {}
+}
+
+(modeule && module.exports && (module.exports = DiGraph));
