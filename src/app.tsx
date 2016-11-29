ada959db5e6d9b434beb2220302d7ec9a8b1e13a
Richard Kichenama <rkichenama@gmail.com>
2016-11-28 23:26:09 -0500

    adjusting render in page

--- src/app.tsx
+++ src/app.tsx
@@ -12,20 +12,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
   for (let i = 0; i < 100; i++)
     arr[i] = Math.floor(Math.random() * 10000) + 1;
 
-  // ReactDOM.render(
-  //   <SortDetailCard list={arr} algorithm={'insertion'} />,
-  //   document.querySelector('#h')
-  // );
-  // ReactDOM.render(
-  //   <SortDetailCard list={arr} algorithm={'selection'} />,
-  //   document.querySelector('#k')
-  // );
   ReactDOM.render(
-    <SortDetailCard list={arr} algorithm={'shell'} />,
+    <SortDetailCard list={arr} algorithm={'insertion'} />,
+    document.querySelector('#h')
+  );
+  ReactDOM.render(
+    <SortDetailCard list={arr} algorithm={'selection'} />,
     document.querySelector('#i')
   );
   ReactDOM.render(
-    <SortDetailCard list={arr} algorithm={'shell_swap'} />,
+    <SortDetailCard list={arr} algorithm={'shell'} />,
     document.querySelector('#j')
+  );
+  ReactDOM.render(
+    <SortDetailCard list={arr} algorithm={'shell_swap'} />,
+    document.querySelector('#k')
   );
 });
