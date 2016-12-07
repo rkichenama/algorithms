6f91710b65cf276db0854193f7dcce277753d4f9
Richard Kichenama <rkichenama@gmail.com>
2016-12-06 23:51:13 -0500

    cpdacy

--- src/components/ListCanvas.tsx
+++ src/components/ListCanvas.tsx
@@ -299,10 +299,14 @@ export class ListCanvas extends React.Component< { algorithm: string, list: any[
 // t: current time, b: begInnIng value, c: change In value, d: duration
 const easeOutBounce = (t, b, c, d) => {
   switch (true) {
-    case ((t /= d) < (1 / 2.75)): return c * (7.5625 * t * t) + b;
-  	case (t < (2 / 2.75)): return c * (7.5625 * (t -= (1.5 / 2.75)) * t + .75) + b;
-  	case (t < (2.5 / 2.75)): return c * (7.5625 * (t -= (2.25 / 2.75)) * t + .9375) + b;
-  	default: return c * (7.5625 * (t -= (2.625 / 2.75)) * t + .984375) + b;
+    case ((t /= d) < (1 / 2.75)):
+      return c * (7.5625 * t * t) + b;
+  	case (t < (2 / 2.75)):
+      return c * (7.5625 * (t -= (1.5 / 2.75)) * t + .75) + b;
+  	case (t < (2.5 / 2.75)):
+      return c * (7.5625 * (t -= (2.25 / 2.75)) * t + .9375) + b;
+  	default:
+      return c * (7.5625 * (t -= (2.625 / 2.75)) * t + .984375) + b;
   }
 };
 
