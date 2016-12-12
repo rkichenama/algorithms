b1573bc6ab94dba920fc136737509b27cbcbf224
Richard Kichenama <rkichenama@gmail.com>
2016-12-12 16:34:57 -0500

    messing around

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from 'react-dom';
 
 import './Tabs.scss';
 
+const TransitionGroup = require('react/lib/ReactTransitionGroup');
+
 export class Tabs extends React.Component<{}, {selected: number}> {
   constructor (props: any, context: any, private unq: string) {
     super(props, context);
@@ -21,8 +23,28 @@ export class Tabs extends React.Component<{}, {selected: number}> {
             <label htmlFor={`${unq}_tab${i}`}>{(!!child.props.title && child.props.title) || 'Tab'}</label>
           ])
         }
-        <article>{this.props.children[this.state.selected]}</article>
+        <Tab>
+          {
+            this.props.children[this.state.selected]
+          }
+        </Tab>
       </article>
     );
   }
 };
+
+class Tab extends React.Component<{ children: any[]}, {}> {
+  private box: any;
+  componentDidMount () { console.info('mounted'); }
+  componentDidUpdate () { console.info('updated'); setTimeout(() => { this.box.style.display = 'block'; this.box.style.opacity = '1'; }, 750); }
+  componentWillReceiveProps () { console.info('receive'); this.box.style.display = 'none'; this.box.style.opacity = '0'; }
+  render () {
+    return (
+      <article ref={d => this.box = d} style={{transition: 'opacity .5s'}}>
+        {
+          this.props.children
+        }
+      </article>
+    );
+  }
+}
